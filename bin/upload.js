9c030ec9e71f1c04d756a4f5b4f201a8dba08253
Joao Moreno <mail@joaomoreno.com>
2014-12-01 18:25:54 +0100

    first steps

---
+++ bin/upload.js
@@ -0,0 +1,13 @@
+var upload = require('../lib/upload');
+
+var argv = require('optimist')
+	.usage('Usage: $0 --account (account) --key (key) --container (container) [--prefix (prefix)] (file1)...')
+	.demand(['account', 'key', 'container'])
+	.argv;
+
+upload(argv.account, argv.key, argv.container, argv.prefix || '', argv._, function (err) {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+});
---
+++ index.js
@@ -0,0 +1,3 @@
+var azure = require('azure-storage');
+
+
